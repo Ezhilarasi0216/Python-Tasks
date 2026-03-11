cfa8bd52ce5a19762d9a8a66a30fc32bb74d5a4c
Ezhilarasi0216 <ezhilguna1602@gmail.com>
2026-03-11 20:58:29 +0530

    ADDED FILE FOR IF CONTITION IN PYTHON

---
+++ PYTHON TASK1.py
@@ -0,0 +1,11 @@
+E=input("enter the number=")
+D=input("enter the number=")
+def value_find(E,D):
+    if E %2==0:
+         print(E,"is even")
+    else:
+         print(E,"is odd")
+    if D %2==0:
+        print(D ,"is even")
+    else:
+        print(D," is odd")
